fc1afb86ed054280b28cbf61c4b637e6f94f3e7c
María Delgado <mariad1996@gmail.com>
2024-04-17 17:30:19 +0200

    POST form finished, re-render Home with each database change left to do

--- src/components/InputCreate.jsx
+++ src/components/InputCreate.jsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 
 const InputCreate = () => {
-    const [text, setText] = useState('')
+    const [text, setText] = useState({ title: '' })
+    const urlPost = 'http://localhost:3000/create'
 
-    const handleSummit = (e) => {
+    const handleOnChange = (e) => {
+        setText({ title: e.target.value })
+    }
+
+    const handleSummit = async (e) => {
         e.preventDefault()
+        await fetch(urlPost, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(text)
+        })
+        setText({ title: '' })
     }
 
     return (
-        <form onSubmit={handleSummit} method='POST' action='/create'>
-            <input type='text' value={text} onChange={e => setText(e.target.value)} />
+        <form onSubmit={handleSummit}>
+            <input type='text' value={text.title} onChange={handleOnChange} />
             <button type='submit'>Añadir</button>
         </form>
     )
