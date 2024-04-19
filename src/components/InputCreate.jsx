9b070fb4c103d2f7a0d92c98ba92f1eb09fbe2bb
María Delgado <mariad1996@gmail.com>
2024-04-19 19:02:31 +0200

    clase

--- src/components/InputCreate.jsx
+++ src/components/InputCreate.jsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
 
 const InputCreate = () => {
-    const [text, setText] = useState({ title: '' })
-    const urlPost = 'http://localhost:3000/create'
+    const [title, setTitle] = useState('')
+    const [res, setRes] = useState('Nada todavía')
 
-    const handleOnChange = (e) => { setText({ title: e.target.value }) }
-    
     const handleSummit = async (e) => {
         e.preventDefault()
-        await fetch(urlPost, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(text)
-        })
-        setText({ title: '' })
-    }
+        const urlPost = 'http://localhost:3000/create'
+        const payload = { title }
 
+        try {
+            const response = await fetch(urlPost, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            })
+            const data = await response.json() // para visualizar lo que se ha enviado
+            setRes(data.title)
+            setTitle('')
+        }
+        catch (error) { console.log(error) }
+    }
+    
     return (
-        <form onSubmit={handleSummit}>
-            <input type='text' value={text.title} onChange={handleOnChange} />
-            <button type='submit'>Añadir</button>
-        </form>
+        <>
+            <form onSubmit={handleSummit}>
+                <input
+                    type='text'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+                <button type='submit'>Añadir</button>
+            </form>
+            <div>
+                Se ha enviado: {res}
+            </div>
+        </>
     )
 }
 
